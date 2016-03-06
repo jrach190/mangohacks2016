0a84efaf9e23fe5e37251f304295d066ee089b38
Arthur HInsvark <ajhinsvark@gmail.com>
2016-03-06 02:10:48 -0500

    Added twitter button and improved flow

--- razorwing.js
+++ razorwing.js
@@ -1,28 +1,4 @@
-$(document).ready(function(){
-    user = "";
-    
-    $('#search').bind('keypress keydown', function(e){
-       if(e.keyCode == 13) { e.preventDefault(); }
-    });
-    
-    
-    
-    $("input[type=text]").keyup(function(e){
-        if(e.keyCode == '13'){
-            user = $("#user_name").val();
-            $('p').text(user);
-            $('img').hide();
-            e.preventDefault();
-            return false;
-        }
-        $(".jumbotron > .container > row > .col-md-4 > form").css("animation-play-state", "running");
-        $(".jumbotron > .container > row > .col-md-4 > img").css({"animation-duration":"2s","animation-name":"query_user_img","animation-direction":"forward","animation-iteration-count":"1","animation-fill-mode":"forward"});
-
-    });
-    
-    
-    $("#submit").click(function(){
-        user = $("#user_name").val();
+function drawGraph(){
         $('#image-container').css('position', 'fixed')
         $('#image-container').remove();
         
@@ -38,8 +14,7 @@ $(document).ready(function(){
             x: -20
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            categories: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
         },
         yAxis: {
             title: {
@@ -52,7 +27,6 @@ $(document).ready(function(){
             }]
         },
         tooltip: {
-            valueSuffix: 'Mood'
         },
         legend: {
             layout: 'vertical',
@@ -61,10 +35,83 @@ $(document).ready(function(){
             borderWidth: 0
         },
         series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            name: user,
+            data: [1, 1, 0, 0, -1, -1, -1]
         }]
     });
+    
+}
+
+
+function pieChart(){
+    $('#graph').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Mood Break-Down'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Positive',
+                y: 50,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Negative',
+                y: 25,
+            }, {
+                name: 'Neutral',
+                y: 25
+            }]
+        }]
+    });
+}
+
+$(document).ready(function(){
+    user = "";
+    
+    $('#search').bind('keypress keydown', function(e){
+       if(e.keyCode == 13) { e.preventDefault(); }
+    });
+    
+    
+    
+    $("input[type=text]").keyup(function(e){
+        if(e.keyCode == '13'){
+            user = $("#user_name").val();
+            drawGraph();
+        }
+        $(".jumbotron > .container > row > .col-md-4 > form").css("animation-play-state", "running");
+        $(".jumbotron > .container > row > .col-md-4 > img").css({"animation-duration":"2s","animation-name":"query_user_img","animation-direction":"forward","animation-iteration-count":"1","animation-fill-mode":"forward"});
+
+    });
+    
+    
+    $("#submit").click(function(){
+        user = $("#user_name").val();
+        drawGraph();
 
     });
     
